b4219cc71e6b2d76a5d304d2976a5c5fcaba49ac
sahasrareddy25 <sahasrareddy25@gmail.com>
2025-12-23 13:58:23 +0530

    Addedfile

---
+++ one.java
@@ -0,0 +1,28 @@
+public class one {
+public int add()
+{
+int a=10;
+int b=20;
+int c=a+b;
+System.out.println(c);
+return c;
+}
+public String name()
+{
+String fname="sahasra";
+return fname;
+}
+public float salary=30000.of;
+return salary;
+}
+}
+public static void main(String[] args){
+One o=new One();
+int result=o.add();
+System.out.println(result);
+String fn=o.name();
+System.out.println(fn);
+float f=0.salary();
+System.out.println(f);
+}
+}
