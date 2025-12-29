2cbfa60d5561754ce7fe39c113601a18f204784d
sahasrareddy25 <sahasrareddy25@gmail.com>
2025-12-29 15:22:34 +0530

    Addedfile

--- loop.java
+++ loop.java
@@ -17,7 +17,7 @@ public class loop{
         System.out.println(fullName);
     }
 }
-*/
+
 
 //all tables
 public class Main {
@@ -37,3 +37,24 @@ public class Main {
         }
     }
 }
+*/
+
+//print 1 to 100 even numbers tables up to 10
+public class Main {
+    public static void main(String[] args) {
+
+        // Outer loop: Even numbers from 2 to 100
+        for (int i = 2; i <= 100; i += 2) {
+
+            System.out.println("Table of " + i);
+
+            // Inner loop: Multiplication from 1 to 10
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(i + " x " + j + " = " + (i * j));
+            }
+
+            System.out.println(); // Blank line between tables
+        }
+    }
+}
+
